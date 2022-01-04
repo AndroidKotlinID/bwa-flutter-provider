64dc13217e50628a508b69aa3a150fa75024a64d
GulajavaMinistudio <gulajava.mini@gmail.com>
2022-01-05 05:13:06 +0700

    Single note screen baru

---
+++ lib/screens/singlenote_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SingleNoteScreen extends StatefulWidget {
+  const SingleNoteScreen({Key? key}) : super(key: key);
+
+  @override
+  _SingleNoteScreenState createState() => _SingleNoteScreenState();
+}
+
+class _SingleNoteScreenState extends State<SingleNoteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
